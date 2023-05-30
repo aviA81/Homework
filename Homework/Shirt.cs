3ce2e3247a80c19efb3c9babe0d1f3ae88e9c6dd
aviA81 <avi81903@gmail.com>
2023-05-30 00:56:14 -0400

    c# quiz

---
+++ Homework/Shirt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks {
+    enum Colors {
+        red,
+        green,
+        blue
+    }
+    enum Designs {
+        striped,
+        checkered,
+        plain
+    }
+    internal class Shirt {
+        public Colors Colors { get; set; }
+        public Designs Designs { get; set; }
+
+        public Shirt(Colors colors, Designs designs) {
+            Colors = colors;
+            Designs = designs;
+        }
+
+        public override string? ToString() {
+            return $"a {Colors} {Designs} shirt";
+        }
+    }
+}
---
+++ Homework/ShirtMaker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks {
+    internal class ShirtMaker {
+        static void Main(string[] args) {
+            Shirt theShirt;
+            foreach (Colors c in Enum.GetValues(typeof(Colors))) {
+                foreach (Designs d in Enum.GetValues(typeof(Designs))) {
+                    theShirt = new Shirt(c, d);
+                    Console.WriteLine(theShirt);
+                }
+            }
+        }
+    }
+}
